218998d77ad8e2b93bad7d2d18746d8db98383f7
bananaplant7 <brooklyncheng2002@gmail.com>
2022-03-10 13:32:53 -0800

    add grid using <table> & loops following youtube vid

---
+++ script.js
@@ -0,0 +1,18 @@
+function makeGrid() { // used: https://www.youtube.com/watch?v=23AGsRkghjo 
+    let tbl = document.getElementById('container');
+
+    for (let i = 1; i <= 16; i++) {
+        let myRow = document.createElement('tr');
+        myRow.id = 'row' + i;
+
+        tbl.appendChild(myRow);
+
+        for (let j = 1; j <= 16; j++) {
+            let myCell = document.createElement('td');
+            myRow.appendChild(myCell);
+        }
+    }
+}
+
+makeGrid();
+
