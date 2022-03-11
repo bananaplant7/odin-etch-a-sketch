d55882eb2c678e51dd513ad30c01ba683e153dde
bananaplant7 <brooklyncheng2002@gmail.com>
2022-03-11 14:16:56 -0800

    add the toggle button effect

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function makeGrid() { // used: https://www.youtube.com/watch?v=23AGsRkghjo
 makeGrid();
 
 const squares = document.querySelectorAll('td');
-squares.forEach(square => square.addEventListener('mouseover', function () {
+squares.forEach(square => square.addEventListener('mouseover', function() {
     this.style.backgroundColor = 'black';
 }));
 
@@ -26,4 +26,18 @@ clear.addEventListener('click', function () {
     squares.forEach(square => square.style.backgroundColor = 'rgb(251, 248, 241)');
 });
 
+
+
+const toggles = document.querySelectorAll('.toggle');
+
+function removeToggle() { //removes toggle-on for all toggles 
+    toggles.forEach(toggle => toggle.classList.remove('toggle-on'))
+}
+
+// remove existing toggle-on & apply toggle-on to 
+// single toggle such that only 1 toggle at a time
+toggles.forEach(toggle => toggle.addEventListener('click', function() {
+    removeToggle() 
+    toggle.classList.add('toggle-on')
+}))
 
