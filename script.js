1960fd29971aeedc4911479db6a02a7e27d10489
bananaplant7 <brooklyncheng2002@gmail.com>
2022-03-11 15:16:41 -0800

    change rgb back to hex, add color picker & color mode functionality

--- script.js
+++ script.js
@@ -13,17 +13,33 @@ function makeGrid() { // used: https://www.youtube.com/watch?v=23AGsRkghjo
         }
     }
 }
-
 makeGrid();
 
+
+
 const squares = document.querySelectorAll('td');
-squares.forEach(square => square.addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'black';
-}));
+const colorPicker = document.querySelector('.color-picker');
+let color = 'black';
+colorPicker.addEventListener('input', function () {
+    color = colorPicker.value;
+});
+
+// turns on sketch pad
+function colorMode() {
+    squares.forEach(square => square.addEventListener('mouseover', function () {
+        this.style.backgroundColor = color;
+    }));
+}
+
+// when colorBtn clicked, turn on sketch pad
+const colorBtn = document.querySelector('.color-btn');
+colorBtn.addEventListener('click', function () {
+    colorMode();
+});
 
 const clear = document.querySelector('.clear-btn');
 clear.addEventListener('click', function () {
-    squares.forEach(square => square.style.backgroundColor = 'rgb(251, 248, 241)');
+    squares.forEach(square => square.style.backgroundColor = '#FBF8F1');
 });
 
 
@@ -31,13 +47,13 @@ clear.addEventListener('click', function () {
 const toggles = document.querySelectorAll('.toggle');
 
 function removeToggle() { //removes toggle-on for all toggles 
-    toggles.forEach(toggle => toggle.classList.remove('toggle-on'))
+    toggles.forEach(toggle => toggle.classList.remove('toggle-on'));
 }
 
 // remove existing toggle-on & apply toggle-on to 
 // single toggle such that only 1 toggle at a time
-toggles.forEach(toggle => toggle.addEventListener('click', function() {
-    removeToggle() 
-    toggle.classList.add('toggle-on')
-}))
+toggles.forEach(toggle => toggle.addEventListener('click', function () {
+    removeToggle();
+    toggle.classList.add('toggle-on');
+}));
 
