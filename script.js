9c86f493df009e601448e0003633bb73b6c3ce05
bananaplant7 <brooklyncheng2002@gmail.com>
2022-03-12 15:23:49 -0800

    add slider func: text showing grid size & grid both update with slider

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-function makeGrid() { // used: https://www.youtube.com/watch?v=23AGsRkghjo 
-    let tbl = document.getElementById('sketchpad');
+let squares = document.querySelectorAll('td');
+function clearGrid() {
+    squares.forEach(square => square.remove())
+}
 
-    for (let i = 1; i <= 16; i++) {
+function makeGrid(x,y) { // used: https://www.youtube.com/watch?v=23AGsRkghjo
+    clearGrid()
+    
+    let tbl = document.getElementById('sketchpad');
+    for (let i = 1; i <= y; i++) {
         let myRow = document.createElement('tr');
         myRow.id = 'row' + i;
 
         tbl.appendChild(myRow);
 
-        for (let j = 1; j <= 16; j++) {
+        for (let j = 1; j <= x; j++) {
             let myCell = document.createElement('td');
             myRow.appendChild(myCell);
         }
     }
+    squares = document.querySelectorAll('td');
 }
-makeGrid();
+makeGrid(16,16)
+
+const sliderValue = document.querySelector('.slider-value');
+const gridSlider = document.querySelector('.grid-slider');
+gridSlider.addEventListener('input', function () {
+    sliderValue.textContent = `${gridSlider.value} x ${gridSlider.value}`;
+    makeGrid(gridSlider.value,gridSlider.value);
+    
+});
 
 
 
-const squares = document.querySelectorAll('td');
 const colorPicker = document.querySelector('.color-picker');
 let color = 'black';
 colorPicker.addEventListener('input', function () {
@@ -55,6 +69,13 @@ clear.addEventListener('click', function () {
     squares.forEach(square => square.style.backgroundColor = 'white');
 });
 
+function randomRGB() { //returns random rgb color
+    let firstRGB = Math.floor(Math.random() * 256);
+    let secondRGB = Math.floor(Math.random() * 256);
+    let thirdRGB = Math.floor(Math.random() * 256);
+    return `rgb(${firstRGB},${secondRGB},${thirdRGB})`;
+}
+
 // turns on rainbowMode
 function rainbowMode() {
     squares.forEach(square => square.addEventListener('mouseover', function () {
@@ -68,23 +89,20 @@ rainbowBtn.addEventListener('click', function () {
     rainbowMode();
 });
 
+
+
 const toggles = document.querySelectorAll('.toggle');
 
-function removeToggle() { //removes toggle-on for all toggles 
+function removeToggle() { //removes toggle-on class from all toggles 
     toggles.forEach(toggle => toggle.classList.remove('toggle-on'));
 }
 
-// remove existing toggle-on & apply toggle-on to 
-// single toggle such that only 1 toggle at a time
+// remove existing toggle-on & apply toggle-on  
+// to single toggle = only 1 toggle at a time
 toggles.forEach(toggle => toggle.addEventListener('click', function () {
     removeToggle();
     toggle.classList.add('toggle-on');
 }));
 
 
-function randomRGB() {
-    let firstRGB = Math.floor(Math.random() * 256);
-    let secondRGB = Math.floor(Math.random() * 256);
-    let thirdRGB = Math.floor(Math.random() * 256);
-    return `rgb(${firstRGB},${secondRGB},${thirdRGB})`;
-}
+
